da7a78f3d02e223098f4c8dbf45e74997806d1a2
qq9068149 <9068149@qq.com>
2021-07-11 01:41:39 +0800

    updata

---
+++ src/views/home.vue
@@ -0,0 +1,66 @@
+<!-- vue 模板 -->
+<template>
+  <div id="home">
+
+    <h1>hello</h1>
+    <div class="test">
+
+    </div>
+    <word></word>
+
+    <hello></hello>
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+
+  created() {
+
+  },
+
+  mounted() {
+
+  },
+
+  methods: {
+
+  },
+
+  computed: {
+
+  },
+
+  watch:{
+
+  },
+
+  filters: {
+
+  },
+
+  // components: {
+  //   word
+  // }
+}
+</script>
+
+<style scoped lang="less">
+  #home{
+    h1{
+      font-size: 14px;
+    }
+    .test{
+      width: 200px;
+      height: 100px;
+      background-color: pink;
+    }
+  }
+</style>
